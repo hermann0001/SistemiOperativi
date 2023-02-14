ebbcac94e21e514f24db1c7dde1cfe4fa01a94dc
hermann0001 <her.tamilia@stud.uniroma3.it>
2023-02-14 16:44:16 +0100

    aggiunto esercizio 2 dei file

---
+++ file/tipi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(){
+	printf("sizeof(pthread_mutex_t)   = %ld\n", sizeof(pthread_mutex_t));
+	printf("sizeof(pthread_barrier_t) = %ld\n", sizeof(pthread_barrier_t));
+	printf("sizeof(pthread_t)         = %ld\n", sizeof(pthread_t));
+	printf("sizeof(size_t)            = %ld\n", sizeof(size_t));
+	printf("sizeof(ssize_t)           = %ld\n", sizeof(ssize_t));
+	printf("sizeof(int)               = %ld\n", sizeof(int));
+	printf("sizeof(unsigned int)      = %ld\n", sizeof(unsigned int));
+	printf("sizeof(long)              = %ld\n", sizeof(long));
+	printf("sizeof(off_t)             = %ld\n", sizeof(off_t));
+	printf("64K in byte               =  %d\n", 1<<16);
+
+	printf("\nEOF = %d\n", EOF);
+}
